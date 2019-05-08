9995f625b03f08440fe36ab723a8c80de1d5a2a7
Ottesen02 <43130155+Ottesen02@users.noreply.github.com>
2019-05-08 20:11:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+
+/* Lånt kode fra W3Schools til vores burgermenu */
 function myFunction(x) {
     x.classList.toggle("change")
 }
 
 
+/* En function som gør at når man scroller ned på vores side bliver vores header sort
+når man så scroller op igen og rammer toppen bliver den transparent igen*/
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
@@ -14,8 +18,8 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
-
-document.querySelector(".burger-menu").addEventListener("click", () => {
+/* Eventlistner som sætter en class på vores mobilmenu og gør vores header sort */
+document.querySelector(".burger-menu").addEventListener("click", () => { 
     document.querySelector(".menu-overlay").classList.toggle("menu-transition");
     document.querySelector("header").style.backgroundColor = "black";
 })
